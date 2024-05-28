92b1b63b58ede0374098538d6f26f9b89a43a604
LagBot5 <167463120+LagBot5@users.noreply.github.com>
2024-05-28 12:30:41 -0400

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -10,16 +10,16 @@ Create table Ark_Creature (
 );
 
 insert into Ark_Creature(species, levels, gender, dino_type, mutation, Age, diets, Dino_id) values
-('Brontosaurus', 100, 'F', 'Land', 15, 'Adolescent', 'Carnivore', 1),
-('Mosasaur', 75, 'F','Sea', 10, 'Adult','Carnivore', 2),
-('Giganotosaurus', 200, 'M', 'Land', 0, 'Egg', 'Carnivore', 3),
-('Carnotaurus', 50, 'M', 'Land', 2, 'Adult','Carnivore', 4),
-('Megatherium', 215, 'F', 'Land', 0, 'Adult', 'Omnivore', 5),
-('Dodo', 15, 'M', 'Land', 0, 'Adult', 'Herbivore', 7),
- ('Wyvern', 68, 'F', 'Sky', 5, 'Adult', 'Carnivore', 8),
-('Pteranodon', 123, 'M', 'Sky', 9, 'Egg', 'Carnivore', 9),
-('Baryonyx', 145, 'F', 'Sea', 3, 'Adolescent', 'Omnivore', 10),
-('Griffon', 100, 'M', 'Sky', 7, 'Adult', 'Carnivore', 11)
+('Brontosaurus', 100, 'F', 'Land', 15, 'Adolescent ', 'Carnivore', 1),
+('Mosasaur', 75, 'F','Sea', 10, 'Adult ','Carnivore', 2),
+('Giganotosaurus', 200, 'M', 'Land', 0, 'Egg ', 'Carnivore', 3),
+('Carnotaurus', 50, 'M', 'Land', 2, 'Adult ','Carnivore', 4),
+('Megatherium', 215, 'F', 'Land', 0, 'Adult ', 'Omnivore', 5),
+('Dodo', 15, 'M', 'Land', 0, 'Adult ', 'Herbivore', 7),
+ ('Wyvern', 68, 'F', 'Sky', 5, 'Adult ', 'Carnivore', 8),
+('Pteranodon', 123, 'M', 'Sky', 9, 'Egg ', 'Carnivore', 9),
+('Baryonyx', 145, 'F', 'Sea', 3, 'Adolescent ', 'Omnivore', 10),
+('Griffon', 100, 'M', 'Sky', 7, 'Adult ', 'Carnivore', 11)
 ;
 
 Create table Main_Purpose(
